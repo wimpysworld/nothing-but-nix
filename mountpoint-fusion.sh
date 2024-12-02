dfe6ea03ca0ef40d9af30d735805de891bab2f74
lucasew <lucas59356@gmail.com>
2024-12-01 21:57:13 -0300

    add missing sudos
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- mountpoint-fusion.sh
+++ mountpoint-fusion.sh
@@ -3,21 +3,21 @@
 root_free_space=$(df -m / | tail -n 1 | awk '{print $2}')
 mnt_free_space=$(df -m /mnt | tail -n 1 | awk '{print $2}')
 
-fallocate -l $((root_free_space - 1024)) /disk.img
-fallocate -l $((mnt_free_space - 1024)) /mnt/disk.img
+sudo fallocate -l $((root_free_space - 1024)) /disk.img
+sudo fallocate -l $((mnt_free_space - 1024)) /mnt/disk.img
 
 
-losetup /dev/loop0 /disk.img
-losetup /dev/loop1 /mnt/disk.img
+sudo losetup /dev/loop0 /disk.img
+sudo losetup /dev/loop1 /mnt/disk.img
 
 # fvck reliability, gotta go fast
-mkfs.btrfs -d raid0 -m raid0 /dev/loop{0,1}
-btrfs device scan
+sudo mkfs.btrfs -d raid0 -m raid0 /dev/loop{0,1}
+sudo btrfs device scan
 
-mkdir -p /state
-mount /dev/loop0 /state -o defaults,noautodefrag,nobarrier,commit=21600,compression=zstd
+sudo mkdir -p /state
+sudo mount /dev/loop0 /state -o defaults,noautodefrag,nobarrier,commit=21600,compression=zstd
 
 for dir in /nix; do
-  mkdir -p {/state,}$dir
-  mount -o bind $dir /state$dir
+  sudo mkdir -p {/state,}$dir
+  sudo mount -o bind $dir /state$dir
 done
