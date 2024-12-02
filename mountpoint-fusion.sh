27dd26c3589cfdb7743f30e36cd9fcd8520db07c
lucasew <lucas59356@gmail.com>
2024-12-01 22:34:39 -0300

    fix montagem espero
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- mountpoint-fusion.sh
+++ mountpoint-fusion.sh
@@ -11,11 +11,11 @@ sudo losetup /dev/loop69 /disk.img
 sudo losetup /dev/loop420 /mnt/disk.img
 
 # fvck reliability, gotta go fast
-sudo mkfs.btrfs -d raid0 -m raid0 /dev/loop{69,420}
+sudo mkfs.btrfs -L actions -d raid0 -m raid0 /dev/loop{69,420}
 sudo btrfs device scan
 
 sudo mkdir -p /state
-sudo mount /dev/loop69 /state -o defaults,noautodefrag,nobarrier,commit=21600,compression=zstd
+sudo mount /dev/loop69 /state -o defaults,noautodefrag,nobarrier,commit=21600,compression=zstd,device=/dev/loop69,device=/dev/loop420
 
 for dir in /nix; do
   sudo mkdir -p {/state,}$dir
